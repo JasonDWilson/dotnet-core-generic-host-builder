bcdd7a3e813991d9aa03b415d6e1ffb8bb2dca5d
Jason Wilson <wilsonj@ausrad.com>
2018-12-05 12:10:07 -0600

    Initial Upload

---
+++ DotNetCore.GenericHostBuilder/GenericHost.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+
+namespace ARA.Applications.Common.Helpers.DotNetCore
+{
+    /// <summary>
+    /// GenericHost class is used to replicate the configuration of Microsoft's WebHost
+    /// in that you can pass it a Startup Class to configure the host and application
+    /// </summary>
+    public static class GenericHost
+    {
+        public static IHostBuilder CreateDefaultBuilder(string[] args)
+        {
+            var builder = new HostBuilder();
+
+            if (args != null)
+            {
+                builder.ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.AddCommandLine(args);
+                });
+                builder.ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddCommandLine(args);
+                });
+            }
+            return builder;
+        }
+
+        public static IHostBuilder UseStartup<TStartupType>(this IHostBuilder builder) where TStartupType : IStartUp
+        {
+            var startup = (TStartupType)Activator.CreateInstance(typeof(TStartupType));
+            builder.ConfigureHostConfiguration(startup.ConfigureHost);
+            builder.ConfigureAppConfiguration(startup.ConfigureApp);
+            builder.ConfigureServices(startup.ConfigureServices);
+            builder.ConfigureLogging(startup.ConfigureLogging);
+            return builder;
+        }
+
+    }
+}
---
+++ DotNetCore.GenericHostBuilder/IStartUp.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace ARA.Applications.Common.Helpers.DotNetCore
+{
+    /// <summary>
+    /// IStartup defines the methods that must be present to configure the
+    /// HostBuilder produced by Generic Host
+    /// </summary>
+    public interface IStartUp
+    {
+        void ConfigureServices(HostBuilderContext context, IServiceCollection services);
+        void ConfigureApp(HostBuilderContext context, IConfigurationBuilder config);
+        void ConfigureHost(IConfigurationBuilder config);
+        void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder);
+    }
+}
